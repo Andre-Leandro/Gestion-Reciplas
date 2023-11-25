f8388fea32e42d40e75c4b9de4e6650ac266652f
SixtoArrejin <128509377+SixtoArrejin@users.noreply.github.com>
2023-11-25 02:28:07 -0300

    update

--- frontend/src/components/BarraLateral.jsx
+++ frontend/src/components/BarraLateral.jsx
@@ -1,9 +1,9 @@
 import {
-    Sidebar,
-    Menu,
-    MenuItem,
-    useProSidebar,
-    SubMenu,
+  Sidebar,
+  Menu,
+  MenuItem,
+  useProSidebar,
+  SubMenu,
 } from "react-pro-sidebar";
 import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
 import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
@@ -17,69 +17,76 @@ import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
 import { Grid } from "@mui/material";
 
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
 
 const capitalizeFirstLetter = (area) => {
   return area.toUpperCase();
 };
 
-
 function BarraLateral({ campos, area }) {
-    const { collapseSidebar } = useProSidebar();
-  
-    return (
-      <div id="sidebar" style={({ height: "100vh" }, { display: "flex" })}>
-        <Sidebar
-          backgroundColor="#eef0f9"
-          rtl={false}
-          style={{ height: "100vh", position: "sticky", top: 0 }}
-        >
-          <Menu>
-            <MenuItem
-              icon={<MenuOutlinedIcon />}
-              onClick={() => {
-                collapseSidebar();
-              }}
-              style={{ textAlign: "center" }}
-            > <h3> {capitalizeFirstLetter(area)} </h3>  </MenuItem>
-  
-            {campos.map((campo) => {
-              if (campo.sub) {
-                return (
-                  <NavLink
-                    to={campo.link}
-                    activeClassName="active"
-                    style={{ color: "black", textDecoration: "none" }}
-                  >
-                    <MenuItem icon={campo.icon}>{campo.nombre}</MenuItem>
-                  </NavLink>
-                );
-              } else {
-                return (
-                  <SubMenu icon={campo.icon} label={campo.nombre}>
-                    {campo.submenus.map((submenu) => (
-                      <Grid marginLeft="5%">
-                        <NavLink
-                          to={submenu.link}
-                          style={{ color: "black", textDecoration: "none" }}
-                        >
-                    
-                            <MenuItem >
-                            • {submenu.nombre}
-                            </MenuItem>
-                        
-                          
-                        </NavLink>
-                      </Grid>
-                    ))}
-                  </SubMenu>
-                );
-              }
-            })}
-          </Menu>
-        </Sidebar>
-      </div>
-    );
+  // const { collapseSidebar } = useProSidebar();
+
+  const [collapsed, setCollapsed] = useState(false)
+  const collapseSidebar = () => {
+    setCollapsed(!collapsed)
   }
-  
-  export default BarraLateral;
-  
+
+  return (
+    <div id="sidebar" style={({ height: "100vh" }, { display: "flex" })}>
+      <Sidebar
+        backgroundColor="#eef0f9"
+        rtl={false}
+        style={{ height: "100vh", position: "sticky", top: 0 }}
+        collapsed={collapsed}
+      >
+        <Menu>
+          <MenuItem
+            icon={<MenuOutlinedIcon />}
+            onClick={() => {
+              // collapseSidebar();
+              collapseSidebar();
+            }}
+            style={{ textAlign: "center" }}
+          > <h3> {capitalizeFirstLetter(area)} </h3>  </MenuItem>
+
+          {campos.map((campo) => {
+            if (campo.sub) {
+              return (
+                <NavLink
+                  to={campo.link}
+                  activeClassName="active"
+                  style={{ color: "black", textDecoration: "none" }}
+                >
+                  <MenuItem icon={campo.icon}>{campo.nombre}</MenuItem>
+                </NavLink>
+              );
+            } else {
+              return (
+                <SubMenu icon={campo.icon} label={campo.nombre}>
+                  {campo.submenus.map((submenu) => (
+                    <Grid marginLeft="5%">
+                      <NavLink
+                        to={submenu.link}
+                        style={{ color: "black", textDecoration: "none" }}
+                      >
+
+                        <MenuItem >
+                          • {submenu.nombre}
+                        </MenuItem>
+
+
+                      </NavLink>
+                    </Grid>
+                  ))}
+                </SubMenu>
+              );
+            }
+          })}
+        </Menu>
+      </Sidebar>
+    </div>
+  );
+}
+
+export default BarraLateral;
+
