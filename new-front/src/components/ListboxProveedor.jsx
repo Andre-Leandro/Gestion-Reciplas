39ad650bc4e03a2f456b4a60fda30877283d3993
Tobias Maciel <tobiasmaciel03@gmail.com>
2023-11-26 21:17:12 -0300

    fix: Datos de ingreso mejorado

--- new-front/src/components/ListboxProveedor.jsx
+++ new-front/src/components/ListboxProveedor.jsx
@@ -1,66 +1,19 @@
 import * as React from "react";
 import TextField from "@mui/material/TextField";
 import Autocomplete from "@mui/material/Autocomplete";
+import Proveedores from "../data/Proveedores.json";
 
 function ListboxProveedor() {
-
   return (
     <Autocomplete
       disablePortal
       id="combo-box-demo"
-      options={proveedores}
+      options={Proveedores}  // Proporciona directamente el arreglo de objetos
+      getOptionLabel={(option) => option.apellido}
       sx={{ width: 300 }}
       renderInput={(params) => <TextField {...params} label="Proveedores" />}
     />
   );
 }
 
-const proveedores = [
-    {
-      label: "PlastiPro",
-    },
-    {
-      label: "EcoPlast",
-    },
-    {
-      label: "Plastic Solutions",
-    },
-    {
-      label: "GreenPlast",
-    },
-    {
-      label: "Plastix",
-    },
-    {
-      label: "EcoFlex",
-    },
-    {
-      label: "Plasticor",
-    },
-    {
-      label: "EcoTech",
-    },
-    {
-      label: "PlastiGreen",
-    },
-    {
-      label: "EcoPlastic",
-    },
-    {
-      label: "Plasticus",
-    },
-    {
-      label: "EcoMold",
-    },
-    {
-      label: "Plasteco",
-    },
-    {
-      label: "EcoPro",
-    },
-    {
-      label: "Plastirec",
-    },
-  ];
-  
 export default ListboxProveedor;
